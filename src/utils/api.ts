1d130ad9f51d28957da1f6574095617b6d0183ec
Brandon Kandola <bkandola98@gmail.com>
2022-11-16 22:01:13 -0500

    add search api func

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,5 +6,15 @@ export const api = axios.create({});
 
 export const getTopMovies = async (page: number = 1) => {
   const response = await api.get(`${apiRoutes.topMoviesURL + page}`);
+  console.log(apiRoutes.searchURL);
   return response.data.results;
+};
+
+export const getMovieBySearch = async (search: string, page: number = 1) => {
+  const response = await api.get(`${apiRoutes.searchURL + search + searchPage()}`);
+  return response.data.results;
+};
+
+export const searchPage = (page: number = 1) => {
+  return `&page=${page}&include_adult=true`;
 };
