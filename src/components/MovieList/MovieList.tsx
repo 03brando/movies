789d216e126949a052a0df70697fa5deea9cb253
Brandon Kandola <bkandola98@gmail.com>
2022-11-17 21:04:59 -0500

    remove stupid buggy infinitre scroll fuck

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,9 +1,8 @@
-import { memo, useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import classnames from 'classnames';
 import { Result } from '../../data/interfaces';
 import { apiRoutes } from '../../data/data';
-import InfiniteScroll from 'react-infinite-scroll-component';
 
 import styles from './MovieList.module.scss';
 import { getPopularMovies, getTopMovies } from '../../utils/api';
@@ -19,49 +18,43 @@ export type Props = {
 function MovieList({ className, title }: Props) {
   const [list, setList] = useState<Result[]>([]);
   const [page, setPage] = useState(1);
+  const dataFetchedRef = useRef(false);
 
-  useEffect(() => {
-    getPopularMovies(page).then((data) => setList(data.results));
+  const getMovies = useCallback(async () => {
+    const movies = await getPopularMovies(page);
+    setList(movies.results);
   }, [page]);
 
-  const fetchMoreData = () => {
-    setTimeout(() => {
-      console.log('Delayed for 5 second.');
-    }, 5000);
-    setPage(page + 1);
-    const prevData = list;
-    getPopularMovies(page).then((data) => setList([...prevData, ...data.results]));
-  };
-
-  const moreMovies = () => {
-    return list.length < 1000;
-  };
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    getMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section className={classnames(styles.MovieList, className)}>
       <h1>{title}</h1>
 
-      <InfiniteScroll dataLength={list.length} next={fetchMoreData} hasMore={moreMovies()} loader={<h4>Loading...</h4>}>
-        <div className={styles.itemWrapper}>
-          {list.map(({ id, title, overview, release_date, poster_path }, index) => (
-            <div className={classnames(styles.item)} key={index}>
-              <div className={styles.wrapper}>
-                <h3 className={styles.title}>{`${title} (${release_date})`}</h3>
-                <p className={styles.overview}>{overview}</p>
-              </div>
-              <div className={styles.imgWrapper}>
-                <Image
-                  src={`${apiRoutes.posterPathURL + poster_path}`}
-                  alt={title}
-                  layout="fill"
-                  loading="lazy"
-                  className={styles.img}
-                />
-              </div>
+      <div className={styles.itemWrapper}>
+        {list.map(({ id, title, overview, release_date, poster_path }, index) => (
+          <div className={classnames(styles.item)} key={index}>
+            <div className={styles.wrapper}>
+              <h3 className={styles.title}>{`${title} (${release_date})`}</h3>
+              <p className={styles.overview}>{overview}</p>
             </div>
-          ))}
-        </div>
-      </InfiniteScroll>
+            <div className={styles.imgWrapper}>
+              <Image
+                src={`${apiRoutes.posterPathURL + poster_path}`}
+                alt={title}
+                layout="fill"
+                loading="lazy"
+                className={styles.img}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
