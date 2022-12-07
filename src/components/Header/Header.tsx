7613b75d4aeb6b85e2b35a0339772ca628ecc2a8
Brandon Kandola <bkandola98@gmail.com>
2022-12-07 02:21:07 -0500

    hamburtger fix and add animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import classnames from 'classnames';
 import { useRouter } from 'next/router';
 import React, { memo, useState } from 'react';
 
@@ -10,33 +9,19 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className={classnames(styles.header)}>
+    <header className={styles.header}>
       <div className={styles.logo}>
         <p>CinematicIndex</p>
       </div>
-      <button
-        aria-label="toggle nav menu"
-        onClick={() => setMenuOpen(!menuOpen)}
-        className={classnames(styles.menuButton, {
-          [styles.menuButtonOpen]: menuOpen
-        })}
-      >
-        <svg
-          viewBox="0 0 20 20"
-          className={classnames(styles.menuSvg, {
-            [styles.menuSvgOpen]: menuOpen
-          })}
-        >
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </button>
-      <div
-        className={classnames(styles.routes, {
-          [styles.routesOpen]: menuOpen
-        })}
-      >
-        {navLinks.map((route) => (
-          <a key={route.path} href={(router.pathname = route.path)}>
+      <div className={`${styles.hamburger} ${menuOpen ? styles.open : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`${styles.routes} ${menuOpen ? styles.open : ''}`}>
+        {navLinks.map((route, key) => (
+          <a key={key} href={(router.pathname = route.path)} onClick={() => setMenuOpen(false)}>
             {route.name}
           </a>
         ))}
