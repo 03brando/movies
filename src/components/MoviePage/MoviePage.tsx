4c5aeddc23bb485096e75782e182943b39c135d1
Brandon Kandola <bkandola98@gmail.com>
2022-12-10 01:43:45 -0500

    add nav animation, page cleanup and more

--- src/components/MoviePage/MoviePage.tsx
+++ src/components/MoviePage/MoviePage.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 
-import { apiRoutes } from '../../data/data';
+import { apiRoutes, moviePage } from '../../data/data';
 import { Movie, Result } from '../../data/interfaces';
 import { getMovieById, getRecommended } from '../../utils/api';
 import Head from '../Head/Head';
@@ -29,20 +29,25 @@ function MoviePage({ id }: Props) {
   const recommendedMovies = useMemo(() => {
     return recommendations && recommendations.length > 0 ? (
       <div className={styles.recommendations}>
-        <h2 className={styles.recommendationsTitle}>Similar Movies</h2>
         {recommendations.map((movie) => (
-          <div
-            key={movie.id}
-            className={styles.recommendation}
-            onClick={() => (window.location.href = `/movies/${movie.id}`)}
-          >
-            <p>{movie.title}</p>
-            <Image src={`${apiRoutes.posterPathURL + movie.poster_path}`} alt={movie.title} width={200} height={300} />
+          <div key={movie.id} className={styles.recommendation} onClick={() => handleClick(movie.id)}>
+            <Image
+              src={`${apiRoutes.posterPathURL + movie.poster_path}`}
+              alt={movie.title}
+              width={200}
+              height={300}
+              className={styles.recImg}
+            />
           </div>
         ))}
       </div>
     ) : null;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [recommendations]);
+
+  const handleClick = useCallback((movieId: number) => {
+    window.location.href = moviePage.route + movieId;
+  }, []);
 
   if (!movie) return null;
 
@@ -50,13 +55,16 @@ function MoviePage({ id }: Props) {
     <main className={styles.MoviePage}>
       <Head title={movie.title} />
       <div className={styles.details}>
-        <h1 className={styles.pageTitle}>{movie.title}</h1>
-        <p className={styles.overview}>{movie.overview}</p>
-        <p className={styles.releaseDate}> </p>
-        <div className={styles.genres}>
-          <p>Genres:</p>
-          {genres}
+        <div className={styles.movieInfo}>
+          <h1 className={styles.pageTitle}>{movie.title}</h1>
+          <p className={styles.overview}>{movie.overview}</p>
+          <p className={styles.releaseDate}> </p>
+          <div className={styles.genres}>
+            <p className={styles.genreText}>Genres:</p>
+            {genres}
+          </div>
         </div>
+        <h2 className={styles.recHeader}>Similar Movies</h2>
         {recommendedMovies}
       </div>
       <div className={styles.imgWrapper}>
